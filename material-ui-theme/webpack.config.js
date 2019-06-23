0d30f76aff25017819c131496e95a51488b5a374
Michal Szczepanski <michal@vane.pl>
2019-06-23 10:33:47 +0200

    Theme material ui base project

---
+++ material-ui-theme/webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const Copy = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/build',
+    filename: 'index.js',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      }
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new Copy([
+      {from: 'html'},
+    ]),
+  ]
+}
