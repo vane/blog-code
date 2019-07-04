4809a5e30a1637b1ecc634716959c6dd3552eee4
Michal Szczepanski <michal@vane.pl>
2019-07-04 17:23:44 +0200

    Chat content basic text input

--- rethinkdb-tornado-graphql-chat-part-2/graphql-frontend/src/view/ChatContent.js
+++ rethinkdb-tornado-graphql-chat-part-2/graphql-frontend/src/view/ChatContent.js
@@ -7,16 +7,33 @@ import { themeStyles } from './customTheme';
 
 export const ChatContent = ({modelState, modelActions}) => {
   const classes = themeStyles();
-
+  const [message, setMessage] = React.useState('');
+  const [messageList, setMessageList] = React.useState([]);
+  const handleKeyPress = (e) => {
+    if (e.charCode === 13) {
+      messageList.push(message);
+      setMessageList(messageList);
+      setMessage('');
+    }
+  }
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  }
   return (<div className={clsx(classes.content, { [classes.contentShift]: modelState.drawerOpen })}>
   <Container>
     <div className={classes.drawerHeader} />
     <div className={classes.chatContent}>
       <div>
-        {}
+        {messageList.map((text, i) => {
+          return <Typography key={`msg_${i}`}>{text}</Typography>
+        })}
       </div>
       <div className={classes.chatInputContainer}>
-        <TextField className={classes.chatInput} variant="outlined" label="Write message" />
+        <TextField className={classes.chatInput}
+                   value={message}
+                   onChange={handleChange}
+                   onKeyPress={handleKeyPress}
+                   variant="outlined" label="Write message" />
       </div>
     </div>
   </Container>
